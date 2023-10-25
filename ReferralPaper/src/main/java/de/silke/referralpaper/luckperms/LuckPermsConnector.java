09aaf65f13f0281be621ed3a451c42c03a2b959a
Silke <53826868+blackysky@users.noreply.github.com>
2023-10-25 14:42:47 +0200

    Добавлено получение LuckPerms роли по UUID

--- ReferralPaper/src/main/java/de/silke/referralpaper/luckperms/LuckPermsConnector.java
+++ ReferralPaper/src/main/java/de/silke/referralpaper/luckperms/LuckPermsConnector.java
@@ -2,9 +2,13 @@ package de.silke.referralpaper.luckperms;
 
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.model.user.UserManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class LuckPermsConnector {
     static RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
@@ -22,6 +26,24 @@ public class LuckPermsConnector {
             LuckPerms api = provider.getProvider();
             User user = api.getUserManager().getUser(player.getUniqueId());
             return user.getPrimaryGroup() != null ? user.getPrimaryGroup() : "default";
+        }
+        return "default";
+    }
+
+    /**
+     * Получить роль игрока по UUID
+     * <p>Если группа не найдена, то по умолчанию будет возвращена группа "default"
+     *
+     * @param playerUUID UUID игрока
+     * @return Роль игрока
+     */
+    @SuppressWarnings("ConstantConditions")
+    public static String getGroup(UUID playerUUID) {
+        if (provider != null) {
+            LuckPerms api = provider.getProvider();
+            UserManager userManager = api.getUserManager();
+            CompletableFuture<User> userFuture = userManager.loadUser(playerUUID);
+            return userFuture.join().getPrimaryGroup() != null ? userFuture.join().getPrimaryGroup() : "default";
         }
         return "default";
     }
